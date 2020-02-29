6badefc06b9870f6e1e2affdad8e753c9cd11173
Crashaholic <33598082+Crashaholic@users.noreply.github.com>
2020-02-29 12:44:46 +0800

    Initial commit

---
+++ AreaRelit/AreaRelit.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using RoR2;
+using UnityEditor;
+using UnityEngine;
+using BepInEx;
+using R2API;
+using UnityEngine.SceneManagement;
+using UnityEngine.Scripting;
+
+namespace AreaRelit
+{
+	[BepInPlugin("com.ohway.areacolor", "Area Recolor", "1.0")]
+	public class AreaRelit : BaseUnityPlugin
+	{
+		public bool enablePanel = false;
+
+		private int clickedR = 0;
+		private int clickedG = 0;
+		private int clickedB = 0;
+
+		public void Awake()
+		{
+			SceneManager.activeSceneChanged += ResetCount;
+			//SceneManager.sceneLoaded += Retint;
+		}
+
+		private void ResetCount(Scene arg0, Scene arg1)
+		{
+			clickedR = clickedG = clickedB = 0;
+		}
+
+		public void Retint(Scene scene, LoadSceneMode mode)
+		{
+			//switch (scene.name)
+			//{
+			//	case "blackbeach2":
+			//	case "blackbeach":
+			//		break;
+			//	case "title":
+			//		Light[] lights = FindObjectsOfType<Light>() as Light[];
+			//		foreach (Light light in lights)
+			//		{
+			//			Color c = new Color(100.0f / 255.0f, 100.0f / 255.0f, 0);
+			//			light.color -= c;
+			//			clickedR = 10;
+			//			clickedB = 10;
+			//		}
+			//		break;
+			//	case "":
+			//		break;
+			//}
+		}
+
+		public void Update()
+		{
+			float dt = Time.deltaTime;
+
+			if (Input.GetKeyDown(KeyCode.F6))
+			{
+				enablePanel = !enablePanel;
+			}
+		}
+
+		public void OnDestroy()
+		{
+			//SceneManager.sceneLoaded -= Retint;
+		}
+
+		public void OnGUI()
+		{
+			if (enablePanel)
+			{
+				if (GUILayout.Button("Reset Lighting"))
+				{
+					Light[] lights = FindObjectsOfType<Light>() as Light[];
+					foreach (Light light in lights)
+					{
+						Color c = new Color(10.0f * clickedR / 255.0f, 10.0f * clickedG / 255.0f, 10.0f * clickedB / 255.0f);
+						light.color -= c;
+					}
+					clickedR = clickedG = clickedB = 0;
+				}
+
+				if (GUILayout.Button(SceneManager.GetActiveScene().name))
+				{ }
+
+				GUILayout.BeginHorizontal();
+				GUILayout.Label(Convert.ToString(clickedR));
+				GUILayout.Label(Convert.ToString(clickedG));
+				GUILayout.Label(Convert.ToString(clickedB));
+				GUILayout.EndHorizontal();
+
+				GUILayout.BeginHorizontal();
+				if (GUILayout.Button("+(10/255, 0, 0) to all lights"))
+				{
+					Light[] lights = FindObjectsOfType<Light>() as Light[];
+					foreach (Light light in lights)
+					{
+						Color c = new Color(10.0f / 255.0f, 0, 0);
+						light.color += c;
+					}
+					clickedR++;
+				}
+
+				if (GUILayout.Button("+(0, 10/255, 0) to all lights"))
+				{
+					Light[] lights = FindObjectsOfType<Light>() as Light[];
+					foreach (Light light in lights)
+					{
+						Color c = new Color(0, 10.0f / 255.0f, 0);
+						light.color += c;
+					}
+					clickedG++;
+				}
+
+				if (GUILayout.Button("+(0, 0, 10/255) to all lights"))
+				{
+					Light[] lights = FindObjectsOfType<Light>() as Light[];
+					foreach (Light light in lights)
+					{
+						Color c = new Color(0, 0, 10.0f / 255.0f);
+						light.color += c;
+					}
+					clickedB++;
+				}
+				GUILayout.EndHorizontal();
+				GUILayout.BeginHorizontal();
+				if (GUILayout.Button("-(10/255, 0, 0) to all lights"))
+				{
+					Light[] lights = FindObjectsOfType<Light>() as Light[];
+					foreach (Light light in lights)
+					{
+						Color c = new Color(10.0f / 255.0f, 0, 0);
+						light.color -= c;
+					}
+					clickedR--;
+				}
+
+				if (GUILayout.Button("-(0, 10/255, 0) to all lights"))
+				{
+					Light[] lights = FindObjectsOfType<Light>() as Light[];
+					foreach (Light light in lights)
+					{
+						Color c = new Color(0, 10.0f / 255.0f, 0);
+						light.color -= c;
+					}
+					clickedG--;
+				}
+
+				if (GUILayout.Button("-(0, 0, 10/255) to all lights"))
+				{
+					Light[] lights = FindObjectsOfType<Light>() as Light[];
+					foreach (Light light in lights)
+					{
+						Color c = new Color(0, 0, 10.0f / 255.0f);
+						light.color -= c;
+					}
+					clickedB--;
+				}
+				GUILayout.EndHorizontal();
+				if (GUILayout.Button("+ (10 / 255) in RGB to all lights"))
+				{
+					Light[] lights = FindObjectsOfType<Light>() as Light[];
+					foreach (Light light in lights)
+					{
+						Color c = new Color(10.0f / 255.0f, 10.0f / 255.0f, 10.0f / 255.0f);
+						light.color += c;
+					}
+					clickedR++;
+					clickedG++;
+					clickedB++;
+				}
+				if (GUILayout.Button("- (10 / 255) in RGB to all lights"))
+				{
+					Light[] lights = FindObjectsOfType<Light>() as Light[];
+					foreach (Light light in lights)
+					{
+						Color c = new Color(10.0f / 255.0f, 10.0f / 255.0f, 10.0f / 255.0f);
+						light.color -= c;
+					}
+					clickedR--;
+					clickedG--;
+					clickedB--;
+				}
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Area Relit
+
+## Description
+A simple relighting mod that I made just to learn the ropes of making a mod. All this mod does is adjust the color values of the lights in the currently loaded scene.
+
+## Usage
+
+|Button | Action|
+|--|--|
+|F6| Opens the Lighting Panel|
+
+## Lighting Panel
+The Lighting Panel will show the name of the current scene as well as buttons to adjust ***all*** the lights in the scene. Each button will add or subtract (10 / 255) to the respective Red, Green or Blue channel color of the lights.
+There is also a button to subtract from all lights as well, to quickly make a scene darker.
